1cfc16de386b7716fac265e0f975d5cd60c8ac45
unknown <shravanimaid2006@gmail.com>
2025-12-02 15:31:04 +0530

    Neelam hell yeahh

--- hell.py
+++ hell.py
@@ -8,11 +8,5 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    if b == 0:
-        return "Division by zero"
     return a / b
 
-add(10,10)
-subtract(20,10)
-multiply(10,10)
-divide(20,20)  	
