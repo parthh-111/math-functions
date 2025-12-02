1a85be16944ce003218f5f0252fd949ad5f1fad1
unknown <shravanimaid2006@gmail.com>
2025-12-02 15:16:25 +0530

    Neelam hell yeahh

--- hell.py
+++ hell.py
@@ -1,4 +1,18 @@
 def add(a, b):
     return a + b
 
-print(add(1, 1))
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Division by zero"
+    return a / b
+
+add(10,10)
+subtract(20,10)
+multiply(10,10)
+divide(20,20)  	
