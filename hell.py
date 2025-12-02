77a147f5dab318c0bc8295eb0d635aee6db1945f
unknown <shravanimaid2006@gmail.com>
2025-12-02 15:31:25 +0530

    Revert "Parth hell yeahh"
    This reverts commit 1cfc16de386b7716fac265e0f975d5cd60c8ac45.

--- hell.py
+++ hell.py
@@ -8,5 +8,11 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    if b == 0:
+        return "Division by zero"
     return a / b
 
+add(10,10)
+subtract(20,10)
+multiply(10,10)
+divide(20,20)  	
